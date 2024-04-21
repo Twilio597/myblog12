4d317aa577b8849d7ae9097d7336a243d300a78c
Somesh <somshptl@gmail.com>
2024-04-21 22:14:14 +0530

    127:Create save Post feature Entity class added 18th April 2024 Test class added, Supplier functional interface example added

--- src/main/java/com/myblog/myblog12/TestClass.java
+++ src/main/java/com/myblog/myblog12/TestClass.java
@@ -6,10 +6,13 @@ import java.util.Random;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println(new Random().nextInt(200));;
+        Supplier<Integer> x = ()->new Random().nextInt(100);
+        Integer y = x.get();
+        System.out.println(y);
     }
 }
