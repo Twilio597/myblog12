05d0aaac4531ce025d19eef2bfa80869c62470b1
Somesh <somshptl@gmail.com>
2024-04-21 22:10:19 +0530

    127:Create save Post feature Entity class added 18th April 2024 Test class added, Supplier functional interface added

--- src/main/java/com/myblog/myblog12/TestClass.java
+++ src/main/java/com/myblog/myblog12/TestClass.java
@@ -2,6 +2,7 @@ package com.myblog.myblog12;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -9,8 +10,6 @@ import java.util.stream.Collectors;
 
 public class TestClass {
     public static void main(String[] args) {
-        List<String> fruits = Arrays.asList("orange", "apple", "banana", "pineapple", "grapes");
-        Consumer<String> val = fruit-> System.out.println(fruit);
-        fruits.forEach(val);
+        System.out.println(new Random().nextInt(200));;
     }
 }
