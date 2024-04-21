4656c83d8ae760b095858411e1da9efb41afc38d
Somesh <somshptl@gmail.com>
2024-04-21 21:46:48 +0530

    127:Create save Post feature Entity class added 18th April 2024 Test class added, function functional interface added

--- src/main/java/com/myblog/myblog12/TestClass.java
+++ src/main/java/com/myblog/myblog12/TestClass.java
@@ -2,19 +2,14 @@ package com.myblog.myblog12;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class TestClass {
     public static void main(String[] args) {
-        List<String> numbers = Arrays.asList("Somu", "ranu", "Somu", "tinku", "vivek", "Somesh");
-        List<String> data1 = numbers.stream().filter(s->s.startsWith("S")).collect(Collectors.toList());
-        List<String> data2 = numbers.stream().filter(s->s.equals("Somu")).collect(Collectors.toList());
-        List<String> data3 = numbers.stream().filter(s->s.endsWith("u")).collect(Collectors.toList());
-        List<String> data4 = numbers.stream().filter(s->s.endsWith("k")).collect(Collectors.toList());
-        System.out.println(data4);
-        System.out.println(data1);
-        System.out.println(data2);
-        System.out.println(data3);
+        List<Integer> data = Arrays.asList(10, 20, 30, 40, 50, 90, 100);
+        List<Integer> newData = data.stream().map(i -> i -5).collect(Collectors.toList());
+        System.out.println(newData);
     }
 }
