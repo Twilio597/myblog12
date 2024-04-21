a172e723a209c85e5cf0710c009b32674a08a7e9
Somesh <somshptl@gmail.com>
2024-04-21 21:59:27 +0530

    127:Create save Post feature Entity class added 18th April 2024 Test class added, Consumer functional interface added

--- src/main/java/com/myblog/myblog12/TestClass.java
+++ src/main/java/com/myblog/myblog12/TestClass.java
@@ -2,14 +2,14 @@ package com.myblog.myblog12;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class TestClass {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(13, 20, 15, 23, 69, 100, 1, 55,69,55,100,20,1,13);
-        List<Integer> distinctNumbers = numbers.stream().distinct().collect(Collectors.toList());
-        System.out.println(distinctNumbers);
+        Consumer<Integer> result = number-> System.out.println(number);
+        result.accept(100);
     }
 }
