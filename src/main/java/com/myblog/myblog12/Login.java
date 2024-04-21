61493f77c7cd1a54873af11f96ab1074153c964e
Somesh <somshptl@gmail.com>
2024-04-21 22:37:35 +0530

    127:Create save Post feature Entity class added 18th April 2024 Test class added, Login class added

---
+++ src/main/java/com/myblog/myblog12/Login.java
@@ -0,0 +1,19 @@
+package com.myblog.myblog12;
+
+public class Login {
+    private String username;
+    private String password;
+
+    public Login(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
