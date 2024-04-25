7ac81ea9db3c7e5eca47286983089037638235d1
Somesh <somshptl@gmail.com>
2024-04-26 00:10:18 +0530

    127:Create save Post feature Entity class added 18th April 2024 Example of groupingBy added

--- src/main/java/com/myblog/myblog12/TestClass.java
+++ src/main/java/com/myblog/myblog12/TestClass.java
@@ -3,14 +3,29 @@ package com.myblog.myblog12;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class TestClass {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(10, 20, 12, 3, 15, 17,18,8,14);
-        List<Integer> data = numbers.stream().filter(n1 -> n1 % 2 == 0).map(n2 -> n2 * n2).collect(Collectors.toList());
-        System.out.println(data);
+        List<Employee> employees = Arrays.asList(
+                new Employee("mike",30,"chennai"),
+                new Employee("adam",25,"chennai"),
+                new Employee("stallin",30,"mumbai"),
+                new Employee("sam",34,"bangalore")
+        );
 
+        Map<Integer, List<Employee>> collect = employees.stream().collect(Collectors.groupingBy(e -> e.getAge()));
+        for(Map.Entry<Integer,List<Employee>> entry:collect.entrySet()){
+            Integer age = entry.getKey();
+            List<Employee> employeesWithAge = entry.getValue();
+            System.out.println("age"+age+"---");
+
+            for(Employee e:employeesWithAge){
+                System.out.println(e.getName());
+                System.out.println(e.getCity());
+            }
+        }
 
     }
 
